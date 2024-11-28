413da60eff5d77015d9af2437d1c2b5f97511853
Fabiuu <168039831+iamfabiuu@users.noreply.github.com>
2024-11-28 18:47:20 -0300

    Update engine.js

--- scripts/engine.js
+++ scripts/engine.js
@@ -6,7 +6,7 @@ const state = {
     score: document.querySelector("#score"),
   },
   values: {
-    gameVelocity: 1000,
+    gameVelocity: 500,
     hitPosition: 0,
     result: 0,
     curretTime: 60,
